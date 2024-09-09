7a2338974e706d8e65173a0fe17d604820c2a2bd
Abel Diress <abeldiress05@gmail.com>
2024-09-09 22:19:17 +0000

    classify header

--- lib/classify.h
+++ lib/classify.h
@@ -2,6 +2,10 @@
 
 namespace libcohere {
   class Classify : private Interface {
-    // 
+    Json classify(const std::vector<std::string> &inputs,
+                  const std::optional<Json> &examples = std::nullopt, // list of objects
+                  const std::optional<string> &model = std::nullopt,
+                  const std::optional<string> &preset = std::nullopt,
+                  const std::string &truncate = "END" /* enum: NONE, START, END */);
   }
 }
